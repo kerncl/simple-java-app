e7a71f41c5947dd9ef98aea659a3b83a4ba628b7
kerncl <kerncl@hotmail.com>
2023-02-27 07:11:18 +0000

    changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is awersome");
+        System.out.println("Github is awersome");
     }
 
     public String getMessage() {
