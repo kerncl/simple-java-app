328228b611a8af7854d0ef50db6d2b548794eca8
kerncl <kerncl@hotmail.com>
2023-02-27 07:41:22 +0000

    typo on jenkins

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "I love Jeekins";
+    private static final String MESSAGE = "I love Jenkins";
 
     public App() {}
 
