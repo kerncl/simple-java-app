61098a005050d3282e46e23e20ff1eb4ebe38bfd
kerncl <kerncl@hotmail.com>
2023-02-27 07:55:24 +0000

    changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Github is awersome");
+        System.out.println("Github is perfect");
     }
 
     public String getMessage() {
